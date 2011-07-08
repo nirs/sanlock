4054913654054b54e37ec919d15157a8ff6bfc4c
David Teigland <teigland@redhat.com>
2011-07-08 11:11:31 -0500

    sanlock: release 1.6

--- src/main.c
+++ src/main.c
@@ -2330,7 +2330,7 @@ static void parse_arg_timeout(char *optstr)
 }
 #endif
 
-#define RELEASE_VERSION "1.5"
+#define RELEASE_VERSION "1.6"
 
 /* 
  * daemon: acquires leases for the local host_id, associates them with a local
