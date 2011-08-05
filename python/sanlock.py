c302c122fffd4008dca5b413a270daea770ef281
Federico Simoncelli <fsimonce@redhat.com>
2011-08-05 13:59:24 +0000

    python: parse lockspaces and resources natively

--- python/sanlock.py
+++ python/sanlock.py
@@ -4,22 +4,5 @@
 # modify, copy, or redistribute it subject to the terms and conditions
 # of the GNU General Public License v.2.
 
-import sys
-import sanlockmod
-
-SANLOCK_FUNCTIONS = (
-    'register', 'add_lockspace', 'rem_lockspace', 'acquire', 'release',
-    'get_alignment'
-)
-
-SanlockException = sanlockmod.exception
-
-for skfun in SANLOCK_FUNCTIONS:
-    setattr(sys.modules[__name__], skfun, getattr(sanlockmod, skfun))
-del skfun
-
-def init_lockspace(lockspace, max_hosts=0, use_aio=True):
-    sanlockmod.init_lockspace(lockspace, max_hosts, 0, use_aio)
-
-def init_resource(resource, num_hosts=0, max_hosts=0, use_aio=True):
-    sanlockmod.init_resource(resource, max_hosts, num_hosts, use_aio)
+# This module will be used to wrap sanlockmod.
+from sanlockmod import *
--- python/sanlockmod.c
+++ python/sanlockmod.c
@@ -12,12 +12,17 @@
 #include <sanlock_admin.h>
 #include <sanlock_direct.h>
 
+#ifndef __unused
+#define __unused __attribute__ ((unused))
+#endif
+
 #define SKERRNO(x) (-x)
 
+/* Sanlock module */
 PyObject *py_module;
 
-/* SANLock exception */
-static PyObject *sanlockmod_exception;
+/* Sanlock exception */
+static PyObject *py_exception;
 
 static void
 __set_exception(int en, char *msg)
@@ -31,59 +36,80 @@ __set_exception(int en, char *msg)
     if (exc_tuple == NULL) {
         PyErr_NoMemory();
     } else {
-        PyErr_SetObject(sanlockmod_exception, exc_tuple);
+        PyErr_SetObject(py_exception, exc_tuple);
         Py_DECREF(exc_tuple);
     }
 }
 
 static int
-__parse_lockspace(char *lockspace, struct sanlk_lockspace *ret_ls)
+__parse_resource(PyObject *obj, struct sanlk_resource **res_ret)
 {
-    int rv;
-    char *lockspace_arg;
+    int i, num_disks, res_len;
+    struct sanlk_resource *res;
 
-    /* sanlock_str_to_lockspace is destructive */
-    lockspace_arg = strdup(lockspace);
+    num_disks = PyList_Size(obj);
 
-    if (lockspace_arg == NULL) {
+    res_len = sizeof(struct sanlk_resource) +
+                        (sizeof(struct sanlk_disk) * num_disks);
+    res = malloc(res_len);
+
+    if (res == NULL) {
         PyErr_NoMemory();
         return -1;
     }
 
-    /* convert lockspace string to structure */
-    rv = sanlock_str_to_lockspace(lockspace_arg, ret_ls);
+    memset(res, 0, res_len);
+    res->num_disks = num_disks;
 
-    if (rv != 0) {
-        __set_exception(SKERRNO(rv), "Invalid SANLock lockspace");
-        goto exit_fail;
+    for (i = 0; i < num_disks; i++) {
+        char *p = NULL;
+        PyObject *tuple, *path = NULL, *offset = NULL;
+
+        tuple = PyList_GetItem(obj, i);
+
+        if (PyTuple_Check(tuple)) {
+            if (PyTuple_Size(tuple) != 2) {
+                __set_exception(EINVAL, "Invalid resource tuple");
+                goto exit_fail;
+            }
+
+            path = PyTuple_GetItem(tuple, 0);
+            offset = PyTuple_GetItem(tuple, 1);
+
+            p = PyString_AsString(path);
+
+            if (!PyInt_Check(offset)) {
+                __set_exception(EINVAL, "Invalid resource offset");
+                goto exit_fail;
+            }
+        } else if (PyString_Check(tuple)) {
+            p = PyString_AsString(tuple);
+        }
+
+        if (p == NULL) {
+            __set_exception(EINVAL, "Invalid resource path");
+            goto exit_fail;
+        }
+
+        strncpy(res->disks[i].path, p, SANLK_PATH_LEN - 1);
+
+        if (offset == NULL) {
+            res->disks[i].offset = 0;
+        } else {
+            res->disks[i].offset = PyInt_AsLong(offset);
+        }
     }
 
-    free(lockspace_arg);
+    *res_ret = res;
     return 0;
 
 exit_fail:
-    free(lockspace_arg);
+    free(res);
     return -1;
 }
 
-static int
-__parse_resource(char *resource, struct sanlk_resource **ret_res)
-{
-    int rv;
-
-    /* convert resource string to structure */
-    rv = sanlock_str_to_res(resource, ret_res);
-
-    if (rv != 0) {
-        __set_exception(SKERRNO(rv), "Invalid SANLock resource");
-        return -1;
-    }
-
-    return 0;
-}
-
 static PyObject *
-py_register(PyObject *self, PyObject *args)
+py_register(PyObject *self __unused, PyObject *args)
 {
     int sanlockfd;
 
@@ -91,29 +117,36 @@ py_register(PyObject *self, PyObject *args)
 
     if (sanlockfd < 0) {
         __set_exception(SKERRNO(sanlockfd),
-                        "SANLock registration failed");
+                        "Sanlock registration failed");
         return NULL;
     }
 
     return PyInt_FromLong(sanlockfd);
 }
 
-static PyObject *py_init_lockspace(PyObject *self, PyObject *args)
+static PyObject *
+py_init_lockspace(PyObject *self __unused, PyObject *args, PyObject *keywds)
 {
-    int rv, max_hosts, num_hosts, use_aio;
-    char *lockspace;
+    int rv, max_hosts = 0, num_hosts = 0, use_aio = 1;
+    const char *lockspace, *path;
     struct sanlk_lockspace ls;
 
+    static char *kwlist[] = {"lockspace", "path", "offest",
+                                "max_hosts", "num_hosts", "use_aio", NULL};
+
+    /* initialize lockspace structure */
+    memset(&ls, 0, sizeof(struct sanlk_lockspace));
+
     /* parse python tuple */
-    if (!PyArg_ParseTuple(args, "siii", &lockspace,
-                          &max_hosts, &num_hosts, &use_aio)) {
+    if (!PyArg_ParseTupleAndKeywords(args, keywds, "ss|kiii", kwlist,
+        &lockspace, &path, &ls.host_id_disk.offset, &max_hosts,
+        &num_hosts, &use_aio)) {
         return NULL;
     }
 
-    /* parse and check sanlock lockspace */
-    if (__parse_lockspace(lockspace, &ls) != 0) {
-        return NULL;
-    }
+    /* prepare sanlock names */
+    strncpy(ls.name, lockspace, SANLK_NAME_LEN);
+    strncpy(ls.host_id_disk.path, path, SANLK_PATH_LEN);
 
     /* init sanlock lockspace (gil disabled) */
     Py_BEGIN_ALLOW_THREADS
@@ -121,29 +154,40 @@ static PyObject *py_init_lockspace(PyObject *self, PyObject *args)
     Py_END_ALLOW_THREADS
 
     if (rv != 0) {
-        __set_exception(SKERRNO(rv), "SANLock lockspace init failure");
+        __set_exception(SKERRNO(rv), "Sanlock lockspace init failure");
         return NULL;
     }
 
     Py_RETURN_NONE;
 }
 
-static PyObject *py_init_resource(PyObject *self, PyObject *args)
+
+static PyObject *
+py_init_resource(PyObject *self __unused, PyObject *args, PyObject *keywds)
 {
-    int rv, max_hosts, num_hosts, use_aio;
-    char *resource;
+    int rv, max_hosts = 0, num_hosts = 0, use_aio = 1;
+    const char *lockspace, *resource;
     struct sanlk_resource *res;
+    PyObject *disks;
+
+    static char *kwlist[] = {"lockspace", "resource", "disks", "max_hosts",
+                                "num_hosts", "use_aio", NULL};
 
     /* parse python tuple */
-    if (!PyArg_ParseTuple(args, "siii", &resource,
-                          &max_hosts, &num_hosts, &use_aio)) {
+    if (!PyArg_ParseTupleAndKeywords(args, keywds, "ssO!|iii",
+        kwlist, &lockspace, &resource, &PyList_Type, &disks, &max_hosts,
+        &num_hosts, &use_aio)) {
         return NULL;
     }
 
     /* parse and check sanlock resource */
-    if (__parse_resource(resource, &res) != 0) {
+    if (__parse_resource(disks, &res) != 0) {
         return NULL;
     }
+
+    /* prepare sanlock names */
+    strncpy(res->lockspace_name, lockspace, SANLK_NAME_LEN);
+    strncpy(res->name, resource, SANLK_NAME_LEN);
 
     /* init sanlock resource (gil disabled) */
     Py_BEGIN_ALLOW_THREADS
@@ -151,29 +195,37 @@ static PyObject *py_init_resource(PyObject *self, PyObject *args)
     Py_END_ALLOW_THREADS
 
     if (rv != 0) {
-        __set_exception(SKERRNO(rv), "SANLock resource init failure");
-        return NULL;
+        __set_exception(SKERRNO(rv), "Sanlock resource init failure");
+        goto exit_fail;
     }
 
+    free(res);
     Py_RETURN_NONE;
+
+exit_fail:
+    free(res);
+    return NULL;
 }
 
 static PyObject *
-py_add_lockspace(PyObject *self, PyObject *args)
+py_add_lockspace(PyObject *self __unused, PyObject *args)
 {
     int rv;
-    char *lockspace;
+    const char *lockspace, *path;
     struct sanlk_lockspace ls;
 
+    /* initialize lockspace structure */
+    memset(&ls, 0, sizeof(struct sanlk_lockspace));
+
     /* parse python tuple */
-    if (!PyArg_ParseTuple(args, "s", &lockspace)) {
+    if (!PyArg_ParseTuple(args, "sks|k",
+        &lockspace, &ls.host_id, &path, &ls.host_id_disk.offset)) {
         return NULL;
     }
 
-    /* parse and check sanlock lockspace */
-    if (__parse_lockspace(lockspace, &ls) != 0) {
-        return NULL;
-    }
+    /* prepare sanlock names */
+    strncpy(ls.name, lockspace, SANLK_NAME_LEN);
+    strncpy(ls.host_id_disk.path, path, SANLK_PATH_LEN);
 
     /* add sanlock lockspace (gil disabled) */
     Py_BEGIN_ALLOW_THREADS
@@ -181,7 +233,7 @@ py_add_lockspace(PyObject *self, PyObject *args)
     Py_END_ALLOW_THREADS
 
     if (rv != 0) {
-        __set_exception(SKERRNO(rv), "SANLock lockspace add failure");
+        __set_exception(SKERRNO(rv), "Sanlock lockspace add failure");
         return NULL;
     }
 
@@ -189,21 +241,24 @@ py_add_lockspace(PyObject *self, PyObject *args)
 }
 
 static PyObject *
-py_rem_lockspace(PyObject *self, PyObject *args)
+py_rem_lockspace(PyObject *self __unused, PyObject *args)
 {
     int rv;
-    char *lockspace;
+    const char *lockspace, *path;
     struct sanlk_lockspace ls;
 
+    /* initialize lockspace structure */
+    memset(&ls, 0, sizeof(struct sanlk_lockspace));
+
     /* parse python tuple */
-    if (!PyArg_ParseTuple(args, "s", &lockspace)) {
+    if (!PyArg_ParseTuple(args, "sks|k",
+        &lockspace, &ls.host_id, &path, &ls.host_id_disk.offset)) {
         return NULL;
     }
 
-    /* parse and check sanlock lockspace */
-    if (__parse_lockspace(lockspace, &ls) != 0) {
-        return NULL;
-    }
+    /* prepare sanlock names */
+    strncpy(ls.name, lockspace, SANLK_NAME_LEN);
+    strncpy(ls.host_id_disk.path, path, SANLK_PATH_LEN);
 
     /* remove sanlock lockspace (gil disabled) */
     Py_BEGIN_ALLOW_THREADS
@@ -211,7 +266,7 @@ py_rem_lockspace(PyObject *self, PyObject *args)
     Py_END_ALLOW_THREADS
 
     if (rv != 0) {
-        __set_exception(SKERRNO(rv), "SANLock lockspace remove failure");
+        __set_exception(SKERRNO(rv), "Sanlock lockspace remove failure");
         return NULL;
     }
 
@@ -219,21 +274,27 @@ py_rem_lockspace(PyObject *self, PyObject *args)
 }
 
 static PyObject *
-py_acquire(PyObject *self, PyObject *args)
+py_acquire(PyObject *self __unused, PyObject *args)
 {
     int rv, sanlockfd;
-    char *resource;
+    const char *lockspace, *resource;
     struct sanlk_resource *res;
+    PyObject *disks;
 
     /* parse python tuple */
-    if (!PyArg_ParseTuple(args, "is", &sanlockfd, &resource)) {
+    if (!PyArg_ParseTuple(args, "issO!",
+        &sanlockfd, &lockspace, &resource, &PyList_Type, &disks)) {
         return NULL;
     }
 
     /* parse and check sanlock resource */
-    if (__parse_resource(resource, &res) != 0) {
+    if (__parse_resource(disks, &res) != 0) {
         return NULL;
     }
+
+    /* prepare sanlock names */
+    strncpy(res->lockspace_name, lockspace, SANLK_NAME_LEN);
+    strncpy(res->name, resource, SANLK_NAME_LEN);
 
     /* acquire sanlock resource (gil disabled) */
     Py_BEGIN_ALLOW_THREADS
@@ -241,7 +302,7 @@ py_acquire(PyObject *self, PyObject *args)
     Py_END_ALLOW_THREADS
 
     if (rv != 0) {
-        __set_exception(SKERRNO(rv), "SANLock resource not acquired");
+        __set_exception(SKERRNO(rv), "Sanlock resource not acquired");
         goto exit_fail;
     }
 
@@ -254,21 +315,27 @@ exit_fail:
 }
 
 static PyObject *
-py_release(PyObject *self, PyObject *args)
+py_release(PyObject *self __unused, PyObject *args)
 {
     int rv, sanlockfd;
-    char *resource;
+    const char *lockspace, *resource;
     struct sanlk_resource *res;
+    PyObject *disks;
 
     /* parse python tuple */
-    if (!PyArg_ParseTuple(args, "is", &sanlockfd, &resource)) {
+    if (!PyArg_ParseTuple(args, "issO!",
+        &sanlockfd, &lockspace, &resource, &PyList_Type, &disks)) {
         return NULL;
     }
 
     /* parse and check sanlock resource */
-    if (__parse_resource(resource, &res) != 0) {
+    if (__parse_resource(disks, &res) != 0) {
         return NULL;
     }
+
+    /* prepare sanlock names */
+    strncpy(res->lockspace_name, lockspace, SANLK_NAME_LEN);
+    strncpy(res->name, resource, SANLK_NAME_LEN);
 
     /* release sanlock resource (gil disabled) */
     Py_BEGIN_ALLOW_THREADS
@@ -276,7 +343,7 @@ py_release(PyObject *self, PyObject *args)
     Py_END_ALLOW_THREADS
 
     if (rv != 0) {
-        __set_exception(SKERRNO(rv), "SANLock resource not released");
+        __set_exception(SKERRNO(rv), "Sanlock resource not released");
         goto exit_fail;
     }
 
@@ -289,10 +356,10 @@ exit_fail:
 }
 
 static PyObject *
-py_get_alignment(PyObject *self, PyObject *args)
+py_get_alignment(PyObject *self __unused, PyObject *args)
 {
     int rv;
-    char *path;
+    const char *path;
     struct sanlk_disk disk;
 
     /* parse python tuple */
@@ -318,21 +385,28 @@ py_get_alignment(PyObject *self, PyObject *args)
 
 static PyMethodDef
 sanlockmod_methods[] = {
-    {"register", py_register, METH_NOARGS, "Register to SANLock daemon."},
-    {"init_lockspace", py_init_lockspace, METH_VARARGS,
-                      "Initialize a device to be used as SANLock lockspace."},
-    {"init_resource", py_init_resource, METH_VARARGS,
-                      "Initialize a device to be used as SANLock resource."},
-    {"add_lockspace", py_add_lockspace, METH_VARARGS,
-                      "Add a lockspace, acquiring a host_id in it."},
-    {"rem_lockspace", py_rem_lockspace, METH_VARARGS,
-                      "Remove a lockspace, releasing our host_id in it."},
-    {"acquire", py_acquire, METH_VARARGS,
-                "Acquire a resource lease for the current process."},
-    {"release", py_release, METH_VARARGS,
-                "Release a resource lease for the current process."},
-    {"get_alignment", py_get_alignment, METH_VARARGS,
-                "Get device alignment."},
+    {"register",
+            py_register, METH_NOARGS, "Register to sanlock daemon."},
+    {"get_alignment",
+            py_get_alignment, METH_VARARGS, "Get device alignment."},
+    {"init_lockspace",
+            (PyCFunction) py_init_lockspace, METH_VARARGS|METH_KEYWORDS,
+            "Initialize a device to be used as sanlock lockspace."},
+    {"init_resource",
+            (PyCFunction) py_init_resource, METH_VARARGS|METH_KEYWORDS,
+            "Initialize a device to be used as sanlock resource."},
+    {"add_lockspace",
+            py_add_lockspace, METH_VARARGS,
+            "Add a lockspace, acquiring a host_id in it."},
+    {"rem_lockspace",
+            py_rem_lockspace, METH_VARARGS,
+            "Remove a lockspace, releasing our host_id in it."},
+    {"acquire",
+            py_acquire, METH_VARARGS,
+            "Acquire a resource lease for the current process."},
+    {"release",
+            py_release, METH_VARARGS,
+            "Release a resource lease for the current process."},
     {NULL, NULL, 0, NULL}
 };
 
@@ -346,7 +420,7 @@ initsanlockmod(void)
         return;
 
     /* Initializing sanlock exception */
-    sanlockmod_exception = PyErr_NewException("sanlockmod.exception", NULL, NULL);
-    Py_INCREF(sanlockmod_exception);
-    PyModule_AddObject(py_module, "exception", sanlockmod_exception);
+    py_exception = PyErr_NewException("sanlockmod.SanlockException", NULL, NULL);
+    Py_INCREF(py_exception);
+    PyModule_AddObject(py_module, "SanlockException", py_exception);
 }
