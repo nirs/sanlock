849c63a095a950a30f79b2f0922d943a794820f0
Federico Simoncelli <fsimonce@redhat.com>
2011-08-05 13:59:25 +0000

    python: add usage example

---
+++ python/example.py
@@ -0,0 +1,41 @@
+import os
+import tempfile
+import sanlock
+
+HOST_ID = 1
+LOCKSPACE_NAME = "lockspace1"
+RESOURCE_NAME = "resource1"
+
+def main():
+    print "Creating the sanlock disk"
+    fd, disk = tempfile.mkstemp()
+    os.close(fd)
+
+    offset = sanlock.get_alignment(disk)
+
+    print "Registering to sanlock"
+    fd = sanlock.register()
+
+    print "Initializing '%s'" % (LOCKSPACE_NAME,)
+    sanlock.init_lockspace(LOCKSPACE_NAME, disk)
+
+    print "Initializing '%s' on '%s'" % (RESOURCE_NAME, LOCKSPACE_NAME)
+    sanlock.init_resource(LOCKSPACE_NAME, RESOURCE_NAME, [(disk, offset)])
+
+    print "Acquiring the id '%i' on '%s'" % (HOST_ID, LOCKSPACE_NAME)
+    sanlock.add_lockspace(LOCKSPACE_NAME, HOST_ID, disk)
+
+    try:
+        print "Acquiring '%s' on '%s'" % (RESOURCE_NAME, LOCKSPACE_NAME)
+        sanlock.acquire(fd, LOCKSPACE_NAME, RESOURCE_NAME, [(disk, offset)])
+        print "Releasing '%s' on '%s'" % (RESOURCE_NAME, LOCKSPACE_NAME)
+        sanlock.release(fd, LOCKSPACE_NAME, RESOURCE_NAME, [(disk, offset)])
+    finally:
+        print "Releasing the id '%i' on '%s'" % (HOST_ID, LOCKSPACE_NAME)
+        sanlock.rem_lockspace(LOCKSPACE_NAME, HOST_ID, disk)
+
+    print "Removing the sanlock disk"
+    os.remove(disk)
+
+if __name__ == '__main__':
+    main()
