1d698a366e0570da50defa9e71bdd1d917b60c28
David Teigland <teigland@redhat.com>
2012-01-17 10:47:53 -0600

    simpler copyright line
    Signed-off-by: David Teigland <teigland@redhat.com>

--- src/sanlock_sock.c
+++ src/sanlock_sock.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2011 Red Hat, Inc.  All rights reserved.
+ * Copyright 2010-2011 Red Hat, Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
