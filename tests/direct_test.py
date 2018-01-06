b03388f11299bc5ba723d61fafb9da4609e8b28a
Nir Soffer <nsoffer@redhat.com>
2018-01-06 03:21:03 +0200

    Start python based tests
    Add README.dev with instructions on how to run the tests, and add couple
    of basic tests for "sanlock direct ...". Testing the direct commands is
    easy. More infrastructure is needed to test the daemon.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

---
+++ tests/direct_test.py
@@ -0,0 +1,33 @@
+"""
+Test sanlock direct options.
+"""
+
+import io
+import struct
+import subprocess
+
+SANLOCK = "src/sanlock"
+
+
+def test_init_lockspace(tmpdir):
+    path = tmpdir.join("lockspace")
+    with io.open(str(path), "wb") as f:
+        # Poison with junk data.
+        f.write(b"x" * 1024**2 + b"X" * 512)
+
+    lockspace = "name:1:%s:0" % path
+    run(SANLOCK, "direct", "init", "-s", lockspace)
+
+    with io.open(str(path), "rb") as f:
+        magic, = struct.unpack("< I", f.read(4))
+        assert magic == 0x12212010
+
+        # TODO: check more stuff here...
+
+        # Do not modify data after the lockspace area.
+        f.seek(1024**2, io.SEEK_SET)
+        assert f.read(512) == b"X" * 512
+
+
+def run(*args):
+    return subprocess.check_output(args)
