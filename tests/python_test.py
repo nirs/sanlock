19a07f6868e22b7026d3dd07638bb33ad898495e
Nir Soffer <nsoffer@redhat.com>
2018-01-15 01:21:52 +0200

    Start python binding test module
    This module allows testing sanlock daemon via the sanlock python
    bindings.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

---
+++ tests/python_test.py
@@ -0,0 +1,27 @@
+"""
+Test sanlock python binding with sanlock daemon.
+"""
+
+import io
+import struct
+
+import sanlock
+
+
+def test_write_lockspace(tmpdir, sanlock_daemon):
+    path = tmpdir.join("lockspace")
+    with io.open(str(path), "wb") as f:
+        # Poison with junk data.
+        f.write(b"x" * 1024**2 + b"X" * 512)
+
+    sanlock.write_lockspace("name", str(path), offset=0)
+
+    with io.open(str(path), "rb") as f:
+        magic, = struct.unpack("< I", f.read(4))
+        assert magic == 0x12212010
+
+        # TODO: check more stuff here...
+
+        # Do not modify data after the lockspace area.
+        f.seek(1024**2, io.SEEK_SET)
+        assert f.read(512) == b"X" * 512
