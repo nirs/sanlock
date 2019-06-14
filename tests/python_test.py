d4964826f5dcd2dc8aa4527acb516acdf24a512b
Nir Soffer <nsoffer@redhat.com>
2019-06-14 20:51:53 +0300

    python: Add missing license blob to python files
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- tests/python_test.py
+++ tests/python_test.py
@@ -1,3 +1,9 @@
+# Copyright (C) 2019 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 """
 Test sanlock python binding with sanlock daemon.
 """
